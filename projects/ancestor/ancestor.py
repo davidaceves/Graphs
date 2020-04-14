d6aa159ab5b9fc04827e331d451f2b222fb38dc0
David <digital0david@gmail.com>
2020-04-13 21:24:17 -0400

    ancestor.py start

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,36 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else: 
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+   graph = Graph()
+
+   for pair in ancestors:
+       parent = pair[0]
+       child = pair[1]
+
+       graph.add_vertex(parent)
+       graph.add_vertex(child)
+       graph.add_edge(parent, child)
+
